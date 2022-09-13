d6fc9ecbd0fb8485cf804f7e15efaf81164cfbab
SyahrulBhudiF <113455742+SyahrulBhudiF@users.noreply.github.com>
2022-09-13 22:33:12 +0700

    Add files via upload

---
+++ Tugas1_Syahrul/Tugas1_Syahrul.java
@@ -0,0 +1,11 @@
+public class Tugas1_Syahrul {
+	public static void main(String[] Args){
+		System.out.print("***** IDENTITAS DIRI *****");
+		System.out.print("\nNama\t:Syahrul Bhudi Ferdiansyah");
+		System.out.print("\nAbsen\t:27");
+		System.out.print("\nKelas\t:1G");
+		System.out.print("\nProdi\t:D-IV Teknik Informatika");
+		System.out.print("\nJurusan\t:Teknologi Informasi");
+	
+	}
+}
